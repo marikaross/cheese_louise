58290651f70733cc699d59644ea7af9cd78bde55
Marika Ross <marika.ross@gmail.com>
2018-07-22 13:04:34 -0600

    Change key for favorites reducer to 'favorites'

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import { cheeseReducer } from './cheeseReducer';
 import { favesReducer } from './favesReducer';
 
-export const rootReducer = () => ({
-  cheese: cheeseReducer,
-  favorites: favoritesReduce
+export const rootReducer = combineReducers({
+  cheeses: cheeseReducer,
+  favorites: favesReducer
 })
