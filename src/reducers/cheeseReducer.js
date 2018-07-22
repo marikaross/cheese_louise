39d97a753f64273189fa92efbe804cd3f87c65d8
Marika Ross <marika.ross@gmail.com>
2018-07-22 13:02:43 -0600

    remove unnecessary import of action

--- src/reducers/cheeseReducer.js
+++ src/reducers/cheeseReducer.js
@@ -1,6 +1,4 @@
-import { addCheese } from '../actions'
-
-export const cheeseReducer = (state = [], action.type) => {
+export const cheeseReducer = (state = [], action) => {
   switch (action.type) {
     case 'ADD_CHEESE': 
       return [...state, ...action.cheeses]
