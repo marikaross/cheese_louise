cb7daaf990751109b0344551294cf41bc521f70e
Marika Ross <marika.ross@gmail.com>
2018-07-22 10:58:25 -0600

    Write reducer for deleteCheese

--- src/reducers/favesReducer.js
+++ src/reducers/favesReducer.js
@@ -1,6 +1,10 @@
 export const favesReducer = (state = [], action) => {
   switch (action.type) {
+    case 'ADD_FAVE-CHEESE':
+      return [...state, action.cheeseId]
+    case 'DELETE_CHEESE':
+      return state.filter(cheese => favorites.cheeseId !== action.cheeseId)
     default:
-    return state
+      return state
   }
 }
