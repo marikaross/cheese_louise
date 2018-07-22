c9f061899401c7e0107faf1106227bd9b96a2189
Marika Ross <marika.ross@gmail.com>
2018-07-22 11:48:20 -0600

    Remove extra App file and wite MDTP and MSTP

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
+import * as apiCalls from '../../helper/api-calls';
+import { cheeseCleaner } from '../../helper/cheese-cleaner';
+import { connect } from 'react-redux';
+import { addCheese } from '../../actions';
 
 
 class App extends Component {
+
+  async componentDidMount() {
+    try {
+    const allCheeseData = await apiCalls.fetchCheese()
+    const cleanCheese = cheeseCleaner(allCheeseData)
+    this.props.addCheese(cleanCheese)
+  } catch (error) {
+      throw new Error (error.message)
+  }
+  }
+
   render() {
     return (
       <div className="App">
+        <h1>Cheese-Louise!</h1>
       </div>
     );
   }
 }
 
 export default App;
+
+export const mapStateToProps = (state) => ({
+  cheese: this.state.cheese
+})
+
+export const mapDispatchToProps = (dispatch) => ({
+  addCheese: (cheeses) => dispatch(addCheese(cheeses)) 
+})
+
+export default
