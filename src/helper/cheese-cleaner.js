943ff98c928c9ffe71fc133773fcac106eb2bc58
Marika Ross <marika.ross@gmail.com>
2018-07-22 13:01:26 -0600

    Remove image path until further notice

--- src/helper/cheese-cleaner.js
+++ src/helper/cheese-cleaner.js
@@ -1,8 +1,8 @@
 export const cheeseCleaner = (cheeseData) => {
   return cheeseData.records.map(cheese => {
-    return { cheese.fields.image.filename
-            name: cheese.fields.cheese
-            milk type: cheese.fields.milk
+    return {cheeseId: cheese.fields.recordid,
+            name: cheese.fields.cheese,
+            milk: cheese.fields.milk,
             region: cheese.fields.id
           };
   });
