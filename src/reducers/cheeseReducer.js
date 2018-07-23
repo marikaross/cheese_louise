6c769e8e4a14d86791600e53bf1ad1d59812cf42
Marika Ross <marika.ross@gmail.com>
2018-07-23 15:48:13 -0600

    Add isLoading, hasErrored, and cheeseFetchDataSuccess as types

--- src/reducers/cheeseReducer.js
+++ src/reducers/cheeseReducer.js
@@ -1,7 +1,27 @@
+export const isLoading = (state = false, action) => {
+  switch (action.type) {
+    case 'IS_LOADING':
+      return action.isLoading
+    default: return state
+  }
+}
+
+export const hasErrored = (state = false, action) => {
+  switch (action.type) {
+    case 'HAS_ERRORED':
+      return action.hasErrored
+    default:
+    return state
+  }
+}
+
+
 export const cheeseReducer = (state = [], action) => {
   switch (action.type) {
     case 'ADD_CHEESE': 
       return [...state, ...action.cheeses]
+    case 'CHEESE_FETCH_DATA_SUCCESS':
+      return action.cheeses
     default:
       return state
   } 
