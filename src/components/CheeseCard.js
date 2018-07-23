edec666fff7e986f5472603afcef456ab6d7e759
Marika Ross <marika.ross@gmail.com>
2018-07-23 15:43:55 -0600

    Pass toggleFave functionality in props

--- src/components/CheeseCard.js
+++ src/components/CheeseCard.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 export const CheeseCard = (props) => {
+
   return (
-    <article key={props.id}>
+    <article className='CheeseCard' key={props.id}>
       <h2>{props.name}</h2>
       <h3>Milk-Type: {props.milk}</h3>
       <h3>Region: {props.region}</h3>
+      <button onClick={() => props.toggleFave(props.id)}>favorite</button>
     </article>
     )
 }
