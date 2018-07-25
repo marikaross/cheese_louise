ea4b5bf6cb555d15b2c68a18e84082c706e597b9
Marika Ross <marika.ross@gmail.com>
2018-07-25 09:41:24 -0600

    Make separate text article for card with classname of cheese-info

--- src/components/CheeseCard.js
+++ src/components/CheeseCard.js
@@ -2,14 +2,18 @@ import React from 'react';
 import './CheeseCard.css'
 
 export const CheeseCard = (props) => {
-
   return (
     <article className='CheeseCard' key={props.id}>
-      <img className='cheese-image' src='https://public.opendatasoft.com/explore/dataset/frenchcheese/files/9829fd7240102fb426d65264161a9390/300/' />
-      <h2>{props.name}</h2>
-      <h3>Milk-Type: {props.milk}</h3>
-      <h3>Region: {props.region}</h3>
-      <button onClick={() => props.toggleFave(props.id)}>favorite</button>
+      <img className='cheese-image' src={require('../images/sad-cheese.jpg')} />
+      <section className='cheese-info'>
+        <h2>{props.name}</h2>
+        <h3>Milk-Type: {props.milk}</h3>
+        <h3>Region: {props.region}</h3>
+        <button onClick={() => props.toggleFave(props.id)}>favorite</button>
+      </section>
     </article>
     )
 }
+
+
+// https://public.opendatasoft.com/explore/dataset/frenchcheese/files/9829fd7240102fb426d65264161a9390/300/
