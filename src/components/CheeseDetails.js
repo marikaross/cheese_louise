edce577a39fe9ffa2295f3e4cd07e91a859740c8
Marika Ross <marika.ross@gmail.com>
2018-07-28 15:26:07 -0600

    Render cheese info

--- src/components/CheeseDetails.js
+++ src/components/CheeseDetails.js
@@ -3,22 +3,20 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { addFaveCheese, deleteCheese } from '../actions';
 
+
 export const CheeseDetails = (props) => {
+console.log(props)
+
   return (
       <div>
+        <img className='cheese-image' src={`https://public.opendatasoft.com/explore/dataset/frenchcheese/files/${props.picture}/300/`} />
+        <h2>{props.name}</h2>
+        <h4>{props.milk}</h4>
+        <h4>{props.region}</h4>
+        <h4>{props.summary}</h4>
+        <Link to='/'>back</Link>
       </div>
     )
 }
 
 
-export const mapStateToProps = (state) => ({
-  cheeses:state.cheeses,
-  favorites:state.favorites
-})
-
-export const mapDispatchToProps = (dispatch) => ({
-  deleteCheese: (id) => dispatch(deleteCheese(id)),
-  addFaveCheese: (id) => dispatch(addFaveCheese(id))
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(CheeseDetails)
