06a51257cdf474617b8c456592826674b31049f0
Marika Ross <marika.ross@gmail.com>
2018-07-28 16:02:13 -0600

    Add setuptests file, re-install enzyme and adapter, write snapshot for cheeseDetails and write proptypes for cheeseDetails

--- src/components/CheeseDetails.js
+++ src/components/CheeseDetails.js
@@ -1,22 +1,28 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { addFaveCheese, deleteCheese } from '../actions';
+import PropTypes from 'prop-types';
 
 
-export const CheeseDetails = (props) => {
-console.log(props)
-
+export const CheeseDetails = ({cheeseId, name, milk, region, summary, picture}) => {
   return (
       <div>
-        <img className='cheese-image' src={`https://public.opendatasoft.com/explore/dataset/frenchcheese/files/${props.picture}/300/`} />
-        <h2>{props.name}</h2>
-        <h4>{props.milk}</h4>
-        <h4>{props.region}</h4>
-        <h4>{props.summary}</h4>
+        <img className='cheese-image' src={`https://public.opendatasoft.com/explore/dataset/frenchcheese/files/${picture}/300/`} />
+        <h2>{name}</h2>
+        <h4>{milk}</h4>
+        <h4>{region}</h4>
+        <h4>{summary}</h4>
         <Link to='/'>back</Link>
       </div>
     )
 }
 
+
+CheeseDetails.propTypes = {
+  name: PropTypes.string,
+  milk: PropTypes.sting,
+  region: PropTypes.string,
+  summary: PropTypes.string,
+  picture: PropTypes.string
+}
 
