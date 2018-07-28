822f1b4e8d7efca1e415904e0db619f1958f60ec
Marika Ross <marika.ross@gmail.com>
2018-07-28 12:11:47 -0600

    Delete fetchSummary function

--- src/helper/api-calls.js
+++ src/helper/api-calls.js
@@ -1,5 +1,7 @@
-export const fetchCheese = async () => {
-  const url = 'https://data.opendatasoft.com/api/records/1.0/search/?dataset=frenchcheese%40public&facet=id&facet=cheese&facet=milk'
+
+
+export const fetchCheeseSummary = async (urlSuffix) => {
+  const url = `https://en.wikipedia.org/w/api.php?action=opensearch&search=${urlSuffix}&limit=1&format=json`
   const response = await fetch(url)
   return response.json()
 }
