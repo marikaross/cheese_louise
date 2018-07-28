1892d75b0546c58c98e4c1c625103bb4dd42aa71
Marika Ross <marika.ross@gmail.com>
2018-07-28 12:12:26 -0600

    Make a summary cleaner

--- src/helper/cheese-cleaner.js
+++ src/helper/cheese-cleaner.js
@@ -6,11 +6,16 @@ export const cheeseCleaner = (cheeseData) => {
     } else {
       picture = '../images/sad-cheese.jpg'
     }
-    return {cheeseId: cheese.recordid,
+    return {frenchWiki: cheese.fields.french_page.substring(cheese.fields.french_page.lastIndexOf('/')),
+            cheeseId: cheese.recordid,
             name: cheese.fields.cheese,
             milk: cheese.fields.milk,
             region: cheese.fields.id,
             picture
-          };
+          }
   });
+}
+
+export const summaryCleaner = (summaryData) => {
+  return summaryData[2][0]
 }
