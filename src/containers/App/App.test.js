e4d96a50cd819972bccddd7c20276773185ded10
Marika Ross <marika.ross@gmail.com>
2018-07-29 17:12:06 -0600

    Add snapshot to App, write tests for MSTP and MDTP in CheeseContainer

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -1,9 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import React from 'react';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App', () => {
+  let wrapper
+  let mockCheeses
+  let mockFavorites
+  let mockIsLoading
+  let mockHasErrored
+  let mockFetchCheese
+  let mockAddCheese
+
+  beforeEach(() => {
+    mockCheeses = [{name: 'nacho', id: 6}, {name: 'swiss', id: 5}]
+    mockFavorites = [{name: 'nacho', id: 6}]
+    mockFetchCheese = jest.fn()
+    mockAddCheese = jest.fn()
+    mockHasErrored = false
+    mockIsLoading = true
+
+     wrapper = shallow(<App 
+      cheeses={mockCheeses}
+      favorites={mockFavorites}
+      isLoading={mockIsLoading}
+      hasErrored={mockHasErrored}
+      addFaveCheese={mockAddCheese}
+      fetchCheese={mockFetchCheese}/>)
+  })
+
+  it('should match the snapShot', () => {
+    expect(wrapper).toMatchSnapshot()
+  })
+})
--- src/containers/CheeseContainer/CheeseContainer.test.js
+++ src/containers/CheeseContainer/CheeseContainer.test.js
@@ -1,11 +1,81 @@
-import { CheeseContainer } from './CheeseContainer';
+import { CheeseContainer, mapStateToProps, mapDispatchToProps } from './CheeseContainer';
 import React from 'react';
 import { shallow } from 'enzyme';
+import * as action from '../../actions';
 
 describe('CheeseContainer', () => {
+  let wrapper
+  let mockCheeses
+  let mockFavorites
+  let mockAddFaveCheese
+  let mockDeleteCheese
+  let mockFetchSummary
+  beforeEach(() => {
+    mockCheeses = [{name: 'nacho', id: 6}, {name: 'swiss', id: 5}]
+    mockFavorites = [{name: 'nacho', id: 6}]
+    mockAddFaveCheese = jest.fn()
+    mockDeleteCheese = jest.fn()
+    wrapper = shallow(
+      <CheeseContainer 
+        cheeses={mockCheeses}
+        favorites={mockFavorites}
+        addFaveCheese={mockAddFaveCheese}
+        deleteCheese={mockDeleteCheese}
+      />)
+  })
 
   it('should match the snapShot', () => {
-    let wrapper = shallow(<CheeseContainer />)
     expect(wrapper).toMatchSnapshot()
   })
-}) 
+
+  describe('mapStateToProps', () => {
+    it('should return a cheeses array, a favorites array', () => {
+      const mockState = {
+        cheeses: mockCheeses,
+        favorites: mockFavorites,
+        addFaveCheese: mockAddFaveCheese
+      }
+
+      const expectedProps = {
+        cheeses: mockCheeses,
+        favorites: mockFavorites
+      }
+
+      const mappedProps = mapStateToProps(mockState)
+      expect(mappedProps).toEqual(expectedProps)
+    })
+  }) 
+
+  describe('mapDispatchToProps', () => {
+    it('calls dispatch with deleteCheese', () => {
+      const mockState = {
+        cheeses: mockCheeses,
+        favorites: mockFavorites,
+        addFaveCheese: mockAddFaveCheese,
+        deleteCheese: mockDeleteCheese
+      }
+
+      const mockDispatch = jest.fn()
+      const mappedProps = mapDispatchToProps(mockDispatch)
+      const actionToDispatch = action.deleteCheese({id: 5})
+      mappedProps.deleteCheese({id: 5})
+      expect(mockDispatch).toBeCalledWith(actionToDispatch)
+    })
+
+    it('calls dispatch with addFaveCheese', () => {
+       const mockState = {
+        cheeses: mockCheeses,
+        favorites: mockFavorites,
+        addFaveCheese: mockAddFaveCheese,
+        deleteCheese: mockDeleteCheese
+      }
+
+      const mockDispatch = jest.fn()
+      const mappedProps = mapDispatchToProps(mockDispatch)
+      const actionToDispatch = action.addFaveCheese({id: 8})
+      mappedProps.addFaveCheese({id: 8})
+      expect(mockDispatch).toBeCalledWith(actionToDispatch)
+    })
+  })
+})
+
