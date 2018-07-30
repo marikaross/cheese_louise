4d3fab8fce1a599676212ca88c455a3ffca99a24
Marika Ross <marika.ross@gmail.com>
2018-07-30 16:29:08 -0600

    Remove milk-type and region props from render

--- src/components/CheeseCard.js
+++ src/components/CheeseCard.js
@@ -19,7 +19,7 @@ export const CheeseCard = (props) => {
       <img className='cheese-image' src={`https://public.opendatasoft.com/explore/dataset/frenchcheese/files/${props.picture}/300/`} />
       <section className='cheese-info'>
         <h2>{props.name}</h2>
-        <button onClick={() => props.toggleFave(props.id)}>favorite</button>
+        <button className='fave-button' onClick={() => props.toggleFave(props.id)}>favorite</button>
         <Link className='details-link'to={`/${props.id}`} onClick={() => makeUrl()}>See more info</Link>
       </section>
     </article>
